a23845df2af7111c948206b4e70dd0748dc0eef8
Tom Radford <tom@radfords-web.com>
2023-02-06 22:26:28 -0400

    Initial Commit

---
+++ FriendsGiving/FriendsGivingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsGivingApp.swift
+//  FriendsGiving
+//
+//  Created by Thomas Radford on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendsGivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
